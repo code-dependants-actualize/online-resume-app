2bb1e4bea9d4e17330bc4073a3ad280a4dc7661e
Allie Neal <allieneal93@gmail.com>
2021-04-19 13:43:54 -0400

    adds another joke

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>{{ joke }}</h1>
+    <h1>{{ joke2 }}</h1>
   </div>
 </template>
 
@@ -13,6 +15,7 @@ export default {
       message:
         "My town is super well known now because of this well-known cartographer that grew up here. He really put us on the map!",
       joke: "Why was the scarecrow awarded a medal? Becuase he was outstanding in his field.",
+      joke2: "What did the bison say to his son when he went to school? 'Bison!'",
     };
   },
   created: function () {},
