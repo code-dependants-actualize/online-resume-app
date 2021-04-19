369488ec18df3599c2358c8a116e46f6d2c8a514
Allie Neal <allieneal93@gmail.com>
2021-04-19 15:25:58 -0400

    adds show page

---
+++ src/views/ShowResume.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>{{ resumeInfo }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      resumeInfo: {},
+    };
+  },
+  created: function () {
+    this.showResume();
+  },
+  methods: {
+    showResume: function () {
+      axios.get("/api/students/" + this.$route.params.id).then((response) => {
+        this.resumeInfo = response.data;
+      });
+    },
+  },
+};
+</script>
